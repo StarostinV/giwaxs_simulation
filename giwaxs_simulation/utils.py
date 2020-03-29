8ed8b529a3efe07b9362d009d59e44e03ec36944
StarostinV <vladimir.starostin@uni-tuebingen.de>
2020-03-29 17:02:43 +0200

    show time only in debug mode

--- giwaxs_simulation/utils.py
+++ giwaxs_simulation/utils.py
@@ -1,13 +1,19 @@
+import logging
+
 from time import perf_counter
+
+logger = logging.getLogger(__name__)
 
 
 def time_counter(func):
+    if logger.level == logging.DEBUG:
+        def wrapper(*args, **kwargs):
+            start = perf_counter()
+            res = func(*args, **kwargs)
+            print(f'Time for {func.__name__} = {(perf_counter() - start):.2f} sec.')
+            print('*' * 10)
+            return res
 
-    def wrapper(*args, **kwargs):
-        start = perf_counter()
-        res = func(*args, **kwargs)
-        print(f'Time for {func.__name__} = {(perf_counter() - start):.2f} sec.')
-        print('*' * 10)
-        return res
-
-    return wrapper
+        return wrapper
+    else:
+        return func
